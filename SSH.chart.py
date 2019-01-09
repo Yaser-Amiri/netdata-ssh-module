bbfb928edce93f7ec115786232d184dfb4011c8b
naitmare01 <david.berndtsson@gmail.com>
2019-01-09 08:13:28 +0100

    Added support for multiple distros

--- SSH.chart.py
+++ SSH.chart.py
@@ -1,8 +1,9 @@
 from re import search
+import platform
 from bases.FrameworkServices.LogService import LogService
 
-update_every = 5
-retries = 3
+UPDATE_EVERY = 5
+RETRIES = 3
 
 
 ORDER = ['failed_authentications']
@@ -20,15 +21,23 @@ CHARTS = {
 class Service(LogService):
     def __init__(self, configuration=None, name=None):
         LogService.__init__(self, configuration=configuration, name=name)
-        self.log_path = self.configuration.get('path', '/var/log/auth.log')
+        if 'Red Hat' in platform.linux_distribution()[0]:
+            log_path = '/var/log/secure'
+        else:
+            log_path = '/var/log/auth.log'
+        self.log_path = self.configuration.get('path', log_path)
         self.order = ORDER
         self.definitions = CHARTS
 
     def _get_data(self):
         try:
             count = 0
+            if 'Red Hat' in platform.linux_distribution()[0]:
+                search_string = 'Failed password for.+ssh'
+            else:
+                search_string = 'Failed password for invalid.+ssh'
             for line in self._get_raw_data():
-                if search(r'Failed password for invalid.+ssh', line):
+                if search(search_string, line):
                     count += 1
             return {"count": count}
         except (ValueError, AttributeError):
